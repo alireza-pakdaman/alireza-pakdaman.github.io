4ef0a65182e01e5d7c7b9ca4180ddb5eceeb5eb8
arshia pakdamanshahri <alirezapakdaman23@gmail.com>
2023-03-24 16:39:41 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,54 +1,48 @@
-$(document).ready(function() {
-  $('.header-image img').animate({
-    opacity: 0.65,
-  }, 0).animate({
-    opacity: 1,
-  }, 1500);
+// Import jQuery
+import $ from "jquery";
 
+// Define a type for the target element
+type TargetElement = HTMLElement | null;
 
-  // Smooth scrolling for internal links
-  $('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if( target.length ) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-    }
-  });
-});
+// Define the smoothScroll function
+function smoothScroll(target: TargetElement, duration: number): void {
+  const targetElement = target as HTMLElement;
+  const targetPosition = targetElement.getBoundingClientRect().top;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  let startTime: number | null = null;
 
-// Smooth Scrolling
-function smoothScroll(target, duration) {
-var target = document.querySelector(target);
-var targetPosition = target.getBoundingClientRect().top;
-var startPosition = window.pageYOffset;
-var distance = targetPosition - startPosition;
-var startTime = null;
+  function animation(currentTime: number): void {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
 
-function animation(currentTime) {
-  if (startTime === null) startTime = currentTime;
-  var timeElapsed = currentTime - startTime;
-  var run = ease(timeElapsed, startPosition, distance, duration);
-  window.scrollTo(0, run);
-  if (timeElapsed < duration) requestAnimationFrame(animation);
+  function ease(t: number, b: number, c: number, d: number): number {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
 }
 
-function ease(t, b, c, d) {
-  t /= d / 2;
-  if (t < 1) return c / 2 * t * t + b;
-  t--;
-  return -c / 2 * (t * (t - 2) - 1) + b;
-}
-
-requestAnimationFrame(animation);
-}
-
-// Smooth scrolling for "Back to Top" button
-var backToTopButton = document.querySelector("#back-to-top");
-
+// Add an event listener for the "Back to Top" button
+const backToTopButton = document.querySelector("#back-to-top");
 if (backToTopButton) {
-backToTopButton.addEventListener("click", function() {
-  smoothScroll("#header", 1000);
-});
+  backToTopButton.addEventListener("click", function () {
+    smoothScroll(document.querySelector("#header"), 1000);
+  });
 }
+
+// Add an event listener for internal links
+$('a[href^="#"]').on('click', function (event) {
+  const target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    smoothScroll(target[0], 1000);
+  }
+});
